c08ad3366304b626de89f5e7372a42bf21c3c570
matsegberts <38218102+matsegberts@users.noreply.github.com>
2018-06-12 18:48:33 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -3,3 +3,11 @@ const ctx = csv.getContext('2d')
 
 let imageName=new Image();
     imageName.src="path/image.png";
+let audioName=new Audio();
+    audioName.src="path/audio.png"; audioName.play();
+
+ctx.drawImage(imageName, X, Y, Width, Height);
+ctx.drawImage(yeet.png, 40, 50, 25, 25);
+
+ctx.fillStyle="red";
+ctx.fillRect= (100, 300, 30, 30);
