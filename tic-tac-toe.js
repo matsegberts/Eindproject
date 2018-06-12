f70155f9d4e4ba66932cd6dda95f152704dccd79
bodeboer <38217941+bodeboer@users.noreply.github.com>
2018-06-12 17:58:42 +0200

    naam effect toe gevoegt

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -15,3 +15,17 @@ else{
    document.beurt = "X";
    }
 }
+
+
+
+
+mouseResponseThreshold = 10;
+
+friction = 0.85;
+
+rotationForce = 0.0;
+
+games = 'Games';
+
+drawName(games);
+bounceBubbles();
