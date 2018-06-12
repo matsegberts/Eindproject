5ac34f0ffa95c17ff5dccd3bceb1ce62efecf5bd
bodeboer <38217941+bodeboer@users.noreply.github.com>
2018-06-12 17:40:07 +0200

    O's zouden moeten werken maar nee

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,4 +4,14 @@ function start(){
 
 function volgende(invullingvak){
   invullingvak.innerText = document.beurt;
+  veranderbeurt();
+}
+
+function veranderbeurt(){
+if(docment.beurt === "X"{
+   docment.beurt = "O";
+}
+else{
+   docment.beurt = "X";
+   }
 }
